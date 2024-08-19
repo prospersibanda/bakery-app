d5b1b95f5980b528f97680a7205fc75a76af489a
prospersibanda <prosper.thecoder@gmail.com>
2024-08-19 23:40:41 +0200

    initial Sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,51 @@
 import React from 'react';
 import { useState } from 'react';
+import Sidebar from './Sidebar';
+import { faHome, faList, faCog} from '@fortawesome/free-solid-svg-icons';
+
+const links = [
+    {
+        name: "Home",
+        path:"/",
+        icon: faHome
+    },
+    {
+        name: "Scones",
+        path:"/scones",
+        icon: faList
+    },
+    {
+        name: "Settings",
+        path:"/settings",
+        icon: faCog
+    }
+]
 const Navbar = () => {
-    const [showSidebar, setShowSidebar] = useState(false)
+    const [showSidebar, setShowSidebar] = useState(false);
+    function closeSidebar(){
+        setShowSidebar(false)
+    }
   return (
-    <div className='navbar container'>
-        <a href='#' className='logo'>Bakery<span>oo</span>Hub</a>
-        <div className='nav-links'>
-            <a href='#' className='active'>Home</a>
-            <a href='#'>Scones</a>
-            <a href='#'>Settings</a>
-        </div>
-        <div onClick={() => setShowSidebar(!showSidebar)} className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}>
-            <div className='bar'></div>
-            <div className='bar'></div>
-            <div className='bar'></div>
-        </div>
-    </div>
+        <>
+            <div className='navbar container'>
+                <a href='#' className='logo'>Bakery<span>oo</span>Hub</a>
+                <div className='nav-links'>
+                    { links.map(link => (
+                        <a href="#!" key={link.name}>{link.name}</a>
+                    )) }
+                    {/* <a href='#' className='active'>Home</a>
+                    <a href='#'>Scones</a>
+                    <a href='#'>Settings</a> */}
+                </div>
+                <div onClick={() => setShowSidebar(true)} className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}>
+                    <div className='bar'></div>
+                    <div className='bar'></div>
+                    <div className='bar'></div>
+                </div>
+            </div>
+            { showSidebar && <Sidebar close={closeSidebar} links={links} /> }
+
+        </>
   )
 }
 
