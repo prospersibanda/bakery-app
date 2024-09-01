8b4153da65de63f75d1128239f4795457593f59c
prospersibanda <prosper.thecoder@gmail.com>
2024-09-02 01:26:05 +0200

    img

--- src/components/CustomImage.jsx
+++ src/components/CustomImage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CustomImage = ({imgSrc, pt}) => {
   return (
     <div className='custom-image' style={{paddingTop: pt}}>
-        <img src={imgSrc}/>
+        <img src={imgSrc} alt='custom image'/>
     </div>
   )
 }
