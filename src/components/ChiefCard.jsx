b5e9c6f1c62b6cc082824d1b67975108d294a890
prospersibanda <prosper.thecoder@gmail.com>
2024-09-01 21:38:54 +0200

    initial chiefs section

--- src/components/ChiefCard.jsx
+++ src/components/ChiefCard.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'; 
 
-const ChiefCard = () => {
+const ChiefCard = ({chief}) => {
   return (
     <div className='chief-card'>
-        <img src="" alt="" />
+        <img src={chief.img} alt="" />
+        <div className='chief-card-info'>
+          <h3 className='chief-name'>{chief.name}</h3>
+          <p className='chief-experience'>Experience: <b>{chief.experience}</b></p>
+          <p className='chief-expertise'>Expertise: <b>{chief.expertise}</b></p>
+          <p className='chief-icons'>
+              <FontAwesomeIcon icon={faFacebook}/>
+              <FontAwesomeIcon icon={faTwitter}/>
+              <FontAwesomeIcon icon={faInstagram}/>
+          </p>
+        </div>
     </div>
   )
 }
--- src/components/ChiefsSection.jsx
+++ src/components/ChiefsSection.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from 'react';
+import ChiefCard from './ChiefCard';
+
 
 const ChiefsSection = () => {
+  const chiefs = [
+    {
+      name: 'Prosper Sibanda',
+      img: '/img/b1.jpg',
+      experience: '10 years',
+      expertise: 'Artisan Baking',
+    },
+    {
+      name: 'Prosper Sibanda',
+      img: '/img/b1.jpg',
+      experience: '10 years',
+      expertise: 'Artisan Baking',
+    },
+    {
+      name: 'Prosper Sibanda',
+      img: '/img/b1.jpg',
+      experience: '10 years',
+      expertise: 'Artisan Baking',
+    },
+    {
+      name: 'Prosper Sibanda',
+      img: '/img/b1.jpg',
+      experience: '10 years',
+      expertise: 'Artisan Baking',
+    },
+    {
+      name: 'Prosper Sibanda',
+      img: '/img/b1.jpg',
+      experience: '10 years',
+      expertise: 'Artisan Baking',
+    },
+    {
+      name: 'Prosper Sibanda',
+      img: '/img/b1.jpg',
+      experience: '10 years',
+      expertise: 'Artisan Bking',
+    },
+  ]
   return (
     <div className='section chiefs'>
         <h1 className='title'>Our Top Chiefs</h1>
         <div className='top-chiefs-container'>
-            
+          { chiefs.map(chief => <ChiefCard key={chief.name} chief={chief}/> ) }
         </div>
     </div>
   )
